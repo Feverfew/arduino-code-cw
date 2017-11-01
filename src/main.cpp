92699f54e764554cc1c54aad6ab5c631f3b520f9
Alexander Saoutkin <a.saoutkin@gmail.com>
2017-11-01 22:14:58 +0000

    Send data over serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
 #include "Arduino.h"
-int strength;
 int val;
 
 void setup()
 {
-    /* pinMode(14, INPUT); */
-    pinMode(11, OUTPUT);
-    /* Serial.begin(9600); */
+    Serial.begin(9600);
 }
 
 void loop()
 {
     val = analogRead(1);
-    strength = map(val, 0, 1023, 0, 255);
     Serial.println(val);
-    analogWrite(11, strength);
+    delay(10);
 }
