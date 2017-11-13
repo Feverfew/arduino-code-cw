3c4e35d6278b966286e46df4ee52df5a8e7bef8c
Alexander Saoutkin <a.saoutkin@gmail.com>
2017-11-13 00:39:13 +0000

    Final Changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,35 @@
 #include "Arduino.h"
 int val;
+int green_led = 2;
+int yellow_led = 8;
+int red_led = 12;
 
 void setup()
 {
     Serial.begin(9600);
+    pinMode(2, OUTPUT);
+    pinMode(8,OUTPUT);
+    pinMode(12, OUTPUT);
 }
 
 void loop()
 {
     val = analogRead(1);
     Serial.println(val);
+    if (Serial.available() > 0) {
+        char score = (char) Serial.read();
+        if (score == 'g') {
+            digitalWrite(green_led, HIGH);
+            digitalWrite(yellow_led, LOW);
+            digitalWrite(red_led, LOW);
+        } else if (score == 'y') {
+            digitalWrite(green_led, LOW);
+            digitalWrite(yellow_led, HIGH);
+            digitalWrite(red_led, LOW);
+        } else {
+            digitalWrite(green_led, LOW);
+            digitalWrite(yellow_led, LOW);
+            digitalWrite(red_led, HIGH);
+        }
+    }
 }
